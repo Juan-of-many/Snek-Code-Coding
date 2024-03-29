9955581467e76df5836467ab21f593f52e381a87
Juan-of-many <fiery.fynx@gmail.com>
2024-03-28 19:24:42 -0800

    confused why it works with slightly different spacing

--- wdd130/w12_fancy_slider/scripts/fancy_slider.js
+++ wdd130/w12_fancy_slider/scripts/fancy_slider.js
@@ -1,37 +1,37 @@
-const track = document.getElementById("image_slider_track");
+const track = document.getElementById("image-track");
 
-const handleOnDown = e => track.dataset.mouseDownAt = e.clientX; //keep track of where the x-value of the mouse is, specifically tracking the starting point from when pressed down.
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
 
 const handleOnUp = () => {
-    track.dataset.mouseDownAt = "0";
-    track.dataset.prevPercentage = track.dataset.percentage;
+  track.dataset.mouseDownAt = "0";  
+  track.dataset.prevPercentage = track.dataset.percentage;
 }
 
 const handleOnMove = e => {
-    if(track.dataset.mouseDownAt === "0") return;
-
-    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-        maxDelta = window.innerwidth / 2;
-
-    const percentage = (mouseDelta / maxDelta) * -100, //calculate the percentage of the slider slid.
-        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage, //take last percentage slid, add it to current percentage, and use it below to get the new percentage
+  if(track.dataset.mouseDownAt === "0") return;
+  
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+  
+  const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
         nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
-
-    track.dataset.percentage = nextPercentage;
-    
-    track.animate({
-        transform: `translate(${nextPercentage}%, -50%)`
+  
+  track.dataset.percentage = nextPercentage;
+  
+  track.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 1200, fill: "forwards" });
+  
+  for(const image of track.getElementsByClassName("image")) {
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`
     }, { duration: 1200, fill: "forwards" });
-
-    for(const image of track.getElementsByClassName("image")) {
-        image.animate({
-            objectPosition: `${100 + nextPercentage}% center`
-        }, { duration: 1200, fill: "forwards" });
-    }
+  }
 }
 
+/* -- Had to add extra lines for touch events -- */
 
-// what are touch events? Whatever they are, this deals with them.
 window.onmousedown = e => handleOnDown(e);
 
 window.ontouchstart = e => handleOnDown(e.touches[0]);
@@ -44,4 +44,4 @@ window.onmousemove = e => handleOnMove(e);
 
 window.ontouchmove = e => handleOnMove(e.touches[0]);
 
-
+// what are touch events? Whatever they are, this deals with them. Also, why does either having different spacing or having notes in green like this cause problems?
